17c897bbec41062a5ca4f273507a1734cc1e370c
ThalesSilva <125931825+ThalesHenriq@users.noreply.github.com>
2024-02-02 19:13:32 -0300

    Update script.js
    adicionado mais questões 😀

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
 const questions = [
     {
-        question: "Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
+        question: "1 - Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
         options: ["Pugilista", "cookie", "surtida", "Sally"],
         correctAnswer: 0
     },
     {
-        question: "Julia Roberts interpreta uma prostituta chamada o que na comédia romântica clássica de 1990 “Uma Linda Mulher”?",
+        question: "2 - Julia Roberts interpreta uma prostituta chamada o que na comédia romântica clássica de 1990 “Uma Linda Mulher”?",
         options: ["violeta", "Victoria", "jenny", "julia"],
         correctAnswer: 0
     },
     {
-        question: "Quem cantou “My Heart Will Go On” no Titanic?",
+        question: "3 - Quem cantou “My Heart Will Go On” no Titanic?",
         options: ["Mariah Carey", "Celine Dion", "Whitney Houston?", "Sally"],
         correctAnswer: 0
     },
-    {
-        question: "Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
-        options: ["Pugilista", "cookie", "surtida", "Sally"],
+     {
+        question: "4 - Qual foi o primeiro filme de terror em cores?",
+        options: ["A maldição de frankenstein", "Casa do demônio ", "O Mistério do Museu de Cera ", "batman"],
         correctAnswer: 0
     },
     {
-        question: "Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
-        options: ["Pugilista", "cookie", "surtida", "Sally"],
+        question: "5 - Qual filme de terror foi a estreia de Johnny Depp? ",
+        options: ["Dark Shadows", "From Hell", "A Nightmare on Elm Street", "Eduard maos de tesoura"],
+        correctAnswer: 2
+    },
+    {
+        question: "6 - Que cor está presente em quase todas as cenas de O Iluminado?",
+        options: ["vermelho", "preto", "verde", "amarelo"],
         correctAnswer: 0
     },
     {
-        question: "Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
-        options: ["Pugilista", "cookie", "surtida", "Sally"],
+        question: "7 - Qual é a famosa citação de O Sexto Sentido?",
+        options: ["Eu vejo pessoas mortas", "Andar por aí como pessoas normais. Eles não se vêem", "Eles só veem o que querem ver", "Eles não sabem que estão mortos."],
         correctAnswer: 0
     },
     {
-        question: "Em Legalmente Loira, qual é o nome do chihuahua de Elle?",
-        options: ["Pugilista", "cookie", "surtida", "Sally"],
+        question: "8 - Que filme de terror apresentou o primeiro banheiro funcionando na tela?",
+        options: ["Psicose (1960)", "Carniçais II (1988) ", "Le Manoir du Diable", "Saw"],
         correctAnswer: 0
+    },
+    {
+        question: "9 - Este filme de terror é baseado em um agente do FBI (Jodie Foster) tentando usar um serial killer canibal (Anthony Hopkins) com doutorado para ajudar a pegar outro serial killer.",
+        options: ["Hannibal", "O Silêncio dos Inocentes", "Red Dragon", "Hanibal"],
+        correctAnswer: 1
+    },
+    {
+        question: "10 - Para que ano Marty e Doc viajam em “De Volta para o Futuro Parte II”?",
+        options: ["2016", "2015", "2012", "2014"],
+        correctAnswer: 1
+    },
+    {
+        question: "11 - Julia Roberts interpreta uma prostituta chamada o que na comédia romântica clássica de 1990 “Uma Linda Mulher”?",
+        options: ["Violeta", "Victoria", "Jenny", "julia"],
+        correctAnswer: 1
     },
     // Adicione mais perguntas aqui...
 ];
